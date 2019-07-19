240eef595500b8a8a4f7818601997cc2ecf43a6f
97 <214951743@qq.com>
2019-07-19 22:04:04 +0800

    master分支回击

--- Hello.java
+++ Hello.java
@@ -1,10 +1,21 @@
 public class Hello{ 
+public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
 	public static void main(String[] args) {
         System.out.println("Hello Word!");
     }
+	
 
 	public void run(){
 		System.out.println("跑一跑!");
+	}
+	pubilc void test(){
+		error("测试一下");
+		System.out.println("测试分支!");
+		System.out.println("master!!!");
+		System.out.println("branch2!!!!");
+
 	}
 	
 	pubilc void test(){
@@ -15,9 +26,12 @@ public class Hello{
 
 	}
 	
+	
 	public void work(){
 		System.out.println("嗨起来!");
-
+public void run(){
+		System.out.println("跑一跑!");
+	}
 	}
 	
 }
